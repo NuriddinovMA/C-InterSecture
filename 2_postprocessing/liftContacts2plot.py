9fbd533cc1dee94d91d92afde5ad5e73c5037059
NuriddinovMA <admin@t03c005.icg.sbras.ru>
2019-03-13 17:23:22 +0700

    some changes in post

--- 2_postprocessing/liftContacts2plot.py
+++ 2_postprocessing/liftContacts2plot.py
@@ -105,11 +105,10 @@ for sm in range(len(Args['samples'])):
 				Map = psf.drawMap(lociCon[l],loci[l],resolution,1,1,3)
 				Ln = len(Map[6])
 				fig, ax = plt.subplots()
-				cmap = matplotlib.cm.ScalarMappable(cmap = 'autumn')
-				sc1 = plt.scatter(Map[0], Map[1], s=Map[2], c=Map[3], cmap = 'autumn')
-				sc1.set_clim(vmin=0, vmax=100)
-				sc2 = plt.scatter(Map[1], Map[0], s=Map[4], c=Map[5], cmap = 'coolwarm')
-				sc2.set_clim(vmin=-100, vmax=100)
+				ax1 = plt.subplot2grid((11,11),(0,0),colspan=8,rowspan=8)
+				sc1 = plt.scatter(Map[1], Map[0], s=Map[4], c=Map[5], cmap = 'coolwarm', vmin=-100, vmax=100)
+				sc2 = plt.scatter(Map[0], Map[1], s=Map[2], c=Map[3], cmap = 'autumn_r', vmin=0, vmax=100)
+				ax1.set_aspect('equal',anchor='W')
 				step = int(pow(Ln,0.5))
 				MarkLines = [ i for i in range(0,Ln,step) ]
 				for i in MarkLines:
@@ -118,29 +117,60 @@ for sm in range(len(Args['samples'])):
 					plt.text(i, i, i+1, withdash=True, size = 12)
 					plt.text(i, i, i+1, withdash=True, size = 12)
 					plt.text(-3, i,i+1, withdash=True, size = 12)
-					plt.text(i, -3, i+1, withdash=True, size = 12)
+					plt.text(i, -2, i+1, withdash=True, size = 12)
 					plt.text(Ln+4, i, i+1, withdash=True, size = 24)
-					plt.text(i, Ln+5, i+1, withdash=True, size = 24)
+					plt.text(i, Ln+6, i+1, withdash=True, size = 24)
 				if Ln < 30: Ln = 30
 				size = Ln/144.0
 				plt.xticks( MarkLines, Map[6][::step], rotation=90, size = 24 )
 				plt.yticks( MarkLines, Map[6][::step], size = 24 )
-				ax.invert_yaxis()
-				ax.xaxis.tick_top()
+				ax1.invert_yaxis()
+				ax1.xaxis.tick_top()
+				
 				del Map
-				plt.subplots_adjust(wspace = 0.05, right = 0.9)
-				ticks = [j for j in range(0,105,5)]
-				cb = plt.colorbar(sc1, ticks = ticks)
-				cb.ax.tick_params(labelsize=24, length = 10, width = 2)
-				plt.subplots_adjust(wspace = 0.05, right = 0.8)
+				ax2 = plt.subplot2grid((12,12),(0,9),rowspan=8)
 				ticks = [j for j in range(-100,105,10)]
-				cb = plt.colorbar(sc2, ticks = ticks)
+				cb = plt.colorbar(sc1, cax=ax2, ticks = ticks)#, pad=0.05, fraction=0.05)
 				cb.ax.tick_params(labelsize=24, length = 10, width = 2)
+				cb.ax.set_ylabel('human - mouse percentile score',fontsize=24 )
+				ax2.set_aspect('auto')
+				
+				ax4 = plt.subplot2grid((12,12),(9,0),colspan=8)
+				ticks = [j for j in range(0,105,5)]
+				cb = plt.colorbar(sc2, cax=ax4, ticks = ticks, orientation='horizontal')#, pad=0.1, fraction=0.05)
+				cb.ax.tick_params(labelsize=24, length = 10, width = 2)
+				cb.ax.set_xlabel('human percentile score',fontsize=24 )
+				ax4.set_aspect('auto')
+				
+				Map = psf.drawStat(1,1,3)
+				ax3 = plt.subplot2grid((12,12),(0,11),rowspan=8)
+				x = np.arange(101)
+				y = np.zeros(101)
+				sc3 = plt.scatter(y[::2]+.5, x[::2], s=Map[1][::2], c=y[::2]-25, cmap = 'coolwarm', vmin=-100, vmax=100)
+				plt.xlim([0,1])
+				plt.xticks([.5,],['Liftovered',], size = 24)
+				plt.yticks(x[::5],x[::5], size = 24)
+				plt.text(1.5,63,'percentile deviation', size=24, rotation = 90)
+				#ax4.invert_xaxis()
+				ax3.yaxis.tick_right()
+				ax3.set_aspect('auto')
+				
+				ax5 = plt.subplot2grid((12,12),(11,0),colspan=8)
+				x = np.arange(101)
+				y = np.zeros(101)
+				sc5 = plt.scatter(x[::2], y[::2]+.5, s=Map[0][::2], c=y[::2]+50, cmap = 'autumn', vmin=0, vmax=100)
+				plt.ylim([0,1])
+				plt.yticks([.5,],['Observed',], size = 24)
+				plt.xticks(x[::5],x[::5], size = 24)
+				plt.xlabel('percentile deviation', size=24)
+				ax5.set_aspect('auto')
+				
 				map = sc1.get_figure()
-				map.set_size_inches((72*size,42*size))
+				map.set_size_inches((48*size,48*size))
+				#plt.tight_layout()
 				plt.savefig( Args['out_path'] + '/%s_%s.%s.%s.%i-%i.map.png' % (Args['out_names'][sm],s[0],s[1],loci[l][0],loci[l][1],loci[l][2]),dpi=100)
 				map.clear()
 				elp = timeit.default_timer() - start_time
 				print '\t\tend plotting %.2f' % elp
-
+elp = timeit.default_timer() - start_time
 print '...end contact comparing', elp
